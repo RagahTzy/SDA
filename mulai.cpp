f5f4035d20a25fc64a526f240a7d289f2c36d3f7
RagahTzy <ragahediting@gmail.com>
2025-05-18 19:41:44 +0700

    Nyoba lagi

--- mulai.cpp
+++ mulai.cpp
@@ -5,7 +5,7 @@ int main(){
   cout << "kasjfoasjdfkasj" << endl;
   cout << "awokaowkoakwoakwok" << endl;
   cout << "MAIN" << endl;
-  cout << "COBA DULU DI COBACOBA" << endl;
+  cout << "COBA DULU DI nyobalagi" << endl;
   
   return 0;
 }
