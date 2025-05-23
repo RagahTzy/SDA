916ba16f91a1a04ed71b7ade868754f6a9de737e
ujangslayer <adzimf3@gmail.com>
2025-05-23 11:03:51 +0700

    pp

--- mulai.cpp
+++ mulai.cpp
@@ -8,6 +8,7 @@ int main(){
   cout << "COBA DULU DI hayyuk" << endl;
   cout << "COBA DULU DI sekalilagi" << endl;
   cout << "JKSHKFJ" << endl;
+  cout << "kasih paham dulu gasieee" << endl;
   
   return 0;
 }
